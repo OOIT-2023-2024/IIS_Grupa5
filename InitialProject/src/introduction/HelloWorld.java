f34bd04918d08276d672f637c17fdecfce1413f0
eLLabTeacher <eLLabTeacher@DESKTOP-S03VIGT>
2024-03-05 10:37:53 +0100

    Druge vezbe - prvi deo

--- InitialProject/src/introduction/HelloWorld.java
+++ InitialProject/src/introduction/HelloWorld.java
@@ -5,6 +5,41 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world");
+
+		// druge vezbe - prvi deo
+		int prviBroj = 7;
+		int drugiBroj;
+		drugiBroj = 2;
+		// int prviBroj=7, drugiBroj=2;
+		System.out.println(prviBroj / drugiBroj); // dobijamo int
+
+		float treciBroj = 5.3f;
+		double cetvrtiBroj = 5.1;
+		System.out.println(treciBroj / cetvrtiBroj); // float ide
+		// u double pa je rezultat double tip podatka
+
+		int petiBroj = 9;
+		System.out.println(petiBroj / cetvrtiBroj); // int ima 32 bita, double ima 64 pa je rezultat double
+		int rezultat = (int) (petiBroj / cetvrtiBroj); // eksplicitna konverzija
+		System.out.println(rezultat);
+
+		boolean alwaysTrue = true;
+		boolean alwaysFalse = false;
+		System.out.println(alwaysTrue && alwaysFalse);
+		System.out.println(alwaysTrue || alwaysFalse);
+		System.out.println(!alwaysTrue);
+
+		char ch = '\n';
+		String prviString = "abc";
+		String drugiString = new String("def");
+		System.out.println(prviString + drugiString);
+		System.out.println(prviString.concat(drugiString));
+		System.out.println(prviString + ch);
+
+		String stringKaoBroj = "5";
+		System.out.println(stringKaoBroj + prviBroj);// int u string
+		// System.out.println((int)stringKaoBroj+prviBroj);
+		System.out.println(Integer.parseInt(stringKaoBroj) + prviBroj);
 	}
 
 }
